a6c313d51f8640c2de1f874f5a3835c5a48509f5
Aaronvikta <aeinsteinvikta@gmail.com>
2025-08-02 17:10:43 +0100

    first axum api

--- hello_main/src/main.rs
+++ hello_main/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use tokio;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+    .route("/", get(hello));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:4500")
+    .await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+
+async fn hello()->&'static str{
+    "Hello, world!"
 }
