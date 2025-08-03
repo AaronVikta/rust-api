f43fefe56bb12a7f1dc336e6cc19ae4fd9c6dca9
Aaronvikta <aeinsteinvikta@gmail.com>
2025-08-03 04:06:06 +0100

    Error Handling

--- hello_main/src/main.rs
+++ hello_main/src/main.rs
@@ -1,14 +1,22 @@
-use axum::{http::StatusCode, routing::get, Json, Router};
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Json, Router};
 use tokio;
 use serde::Serialize;
+use anyhow::Context;
+
+
 #[tokio::main]
-async fn main() {
+async fn main()-> anyhow::Result<()> {
     let app = Router::new()
-    .route("/", get(hello_json));
+    .route("/", get(hello_json))
+    .layer(tower_http::catch_panic::CatchPanicLayer::new());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:4500")
-    .await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    .await
+    .context("Failed to bind TCP Listener")?;
+    axum::serve(listener, app).await
+    .context("Axum::serve filed")?;
+
+    Ok(())
 }
 
 
@@ -16,14 +24,38 @@ async fn main() {
 //     "Hello, world!"
 // }
 
+struct AppError(anyhow::Error);
+
+impl From<anyhow::Error> for AppError{
+    fn from(value: anyhow::Error) -> Self{
+        Self(value)
+    }
+}
+
+impl IntoResponse for AppError{
+    fn into_response(self) -> axum::response::Response {
+        (StatusCode::INTERNAL_SERVER_ERROR,
+        self.0.to_string()).into_response()
+    }
+}
+
 #[derive(Serialize)]
 struct Response {
     message: &'static str,
 }
 
-async fn hello_json() ->(StatusCode, Json<Response>){
-    let response = Response{
-        message: "Hello,World!",
+async fn hello_json() -> Result<(StatusCode, Json<Response>), AppError> {
+    let response = Response {
+        message: generate_message().context("failed to generate message")?,
     };
-    (StatusCode::OK, Json(response))
+
+    Ok((StatusCode::OK, Json(response)))
+}
+
+
+fn generate_message()->anyhow::Result<&'static str>{
+    if rand::random(){
+        anyhow::bail!("No Message for You");
+    }
+    Ok("Hello, World!")
 }
