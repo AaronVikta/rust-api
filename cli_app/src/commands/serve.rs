ac4568a46c5fb8b3f4509ed72599537d19368035
Aaronvikta <aeinsteinvikta@gmail.com>
2025-08-11 05:26:02 +0100

    CLI args

---
+++ cli_app/src/commands/serve.rs
@@ -0,0 +1,24 @@
+use clap::{ArgMatches, Command,Arg, value_parser};
+
+pub const COMMAND_NAME: &str = "serve";
+
+pub fn configure() -> Command {
+    Command::new(COMMAND_NAME).about("start HTTP server").arg(
+        Arg::new("port")
+            .short('p')
+            .long("port")
+            .value_name("PORT")
+            .help("TCP port to listen on")
+            .default_value("8080")
+            .value_parser(value_parser!(u16))
+    )
+}
+
+pub fn handle(matches: &ArgMatches) -> anyhow::Result<()> {
+
+    let port: u16 = *matches.get_one("port").unwrap_or(&8080);
+ 
+    println!("TBD: start the webserver on port {} ",port);
+
+    Ok(())
+}
---
+++ cli_app/src/main.rs
@@ -0,0 +1,15 @@
+#[allow(unused_imports)]
+use clap::{Arg,Command};
+use cli_app::commands;
+
+
+pub fn main()->anyhow::Result<()>{
+
+    let mut command = Command::new("Sample CLI application");
+    command = commands::configure(command);
+
+    let matches  = command.get_matches();
+    commands::handle(&matches)?;
+
+    Ok(())
+}
