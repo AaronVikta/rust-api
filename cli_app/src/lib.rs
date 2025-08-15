b1c2b0eb1373e12c62ce74fa165ba996dea1198a
Aaron Victor <aeinsteinvikta@gmail.com>
2025-08-15 22:33:11 +0100

    API Services

--- cli_app/src/lib.rs
+++ cli_app/src/lib.rs
@@ -2,3 +2,5 @@ pub mod commands;
 pub mod settings;
 pub mod api;
 pub mod state;
+pub mod model;
+pub mod services;
--- cli_app/src/state/mod.rs
+++ cli_app/src/state/mod.rs
@@ -1,15 +1,20 @@
-use crate::settings::Settings;
+use crate::{services::{post::InMemoryPostService, user::InMemoryUserService}, settings::Settings};
 use std::sync::Arc;
 use arc_swap::ArcSwap;
 
 
 pub struct ApplicationState{
-    pub settings: ArcSwap<Settings>
+    pub settings: ArcSwap<Settings>,
+    pub user_service: Arc<InMemoryUserService>,
+    pub post_service:Arc<InMemoryPostService>
 }
 
 impl ApplicationState{
     pub fn new(settings:&Settings)->anyhow::Result<Self>{
-        Ok(Self { settings: ArcSwap::new(Arc::new((*settings).clone())),
+        Ok(Self { 
+            settings: ArcSwap::new(Arc::new((*settings).clone())),
+            user_service: Arc::new(InMemoryUserService::default()),
+            post_service: Arc::new(InMemoryPostService::default())
          })
     }
 }
